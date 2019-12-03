2ec7c7933b2d1db651bce247e23df3ff796374f6
Tom Kemper <tomkempernl@gmail.com>
2019-12-03 21:29:10 +0100

    Day 2B

--- AoC2019.Tests/Day2Tests.fs
+++ AoC2019.Tests/Day2Tests.fs
@@ -4,22 +4,29 @@ open NUnit.Framework
 open FsUnit
 open System.IO
 
-
 let input =     
     File.ReadAllText("./Week2InputA.txt").Split(',') 
         |> Array.map System.Int32.Parse
 
-[<Test>]
-let ``Day2 Example`` () = 
-    run [|1;0;0;0;99|] 0 |> should equivalent [|2;0;0;0;99|]
-    run [|2;3;0;3;99|] 0 |> should equivalent [|2;3;0;6;99|]
-    run [|2;4;4;5;99;0|] 0 |> should equivalent [|2;4;4;5;99;9801|]
-    run [|1;1;1;4;99;5;6;0;99|] 0 |> should equivalent [|30;1;1;4;2;5;6;0;99|]
-    run [|1;9;10;3;2;3;11;0;99;30;40;50|] 0 |> should equivalent [|3500;9;10;70;2;3;11;0;99;30;40;50|]
+let test input = 
+    run (Intcode input) 0 |> fun (Intcode x) -> x 
 
 [<Test>]
-let ``Day2 A`` () =
-    Array.set input 1 12
-    Array.set input 2 2
-    let result = run input 0
-    Array.get result 0 |> should equal 4138658
+let ``Day2 Example`` () =     
+    test [|1;0;0;0;99|] |> should equivalent [|2;0;0;0;99|]
+    test [|2;3;0;3;99|] |> should equivalent [|2;3;0;6;99|]
+    test [|2;4;4;5;99;0|] |> should equivalent [|2;4;4;5;99;9801|]
+    test [|1;1;1;4;99;5;6;0;99|] |> should equivalent [|30;1;1;4;2;5;6;0;99|]
+    test [|1;9;10;3;2;3;11;0;99;30;40;50|] |> should equivalent [|3500;9;10;70;2;3;11;0;99;30;40;50|]
+
+[<Test>]
+let ``Day2 A`` () =    
+    runValue (Intcode input) 12 2 |> should equal 4138658
+
+[<Test>]
+let ``Day2 B, by brute force...`` () =    
+    match (bruteForce (Intcode input) 19690720) with
+    | Some (n,v) -> 
+        let result = (100 * n) + v
+        result |> should equal 7264
+    | None -> failwith "nope"
--- AoC2019/Day2.fs
+++ AoC2019/Day2.fs
@@ -1,18 +1,63 @@
 module AoC2019.Day2
+open AoC2019.Shared
 
-let rec run (input: int array) pos = 
-    let get = Array.get input
+type Intcode = Intcode of int array
+type Opcode = 
+    | Halt
+    | Add
+    | Multiply
+
+let mapping = dict [
+    (99, Halt);
+    (1, Add);
+    (2, Multiply)]
+
+let arity = dict [
+    (Halt, 0);
+    (Add, 3);
+    (Multiply, 3)
+]
+
+let rec run (Intcode input) pos =     
     let set = Array.set input
+    let get = Array.get input
+    let op x = 
+        let op =  mapping.[get pos]
+        let arity = arity.[op]
+        if arity > 0 then        
+            (op, Array.sub input (pos + 1) arity, pos + 1 + arity)
+        else 
+            (op, [||], pos)
 
-    match get pos with
-    | 99 -> input
-    | _ ->
-        let items = Array.sub input pos 4        
-        match items with
-        | [|1; x;y;p|] ->
-            set p (get x + get y) |> ignore
-            run input (pos+4)
-        | [|2; x;y;p|] ->
-            set p (get x * get y) |> ignore
-            run input (pos+4)
-        | _ -> failwith "Nope"
+    match op pos with
+    | (Halt, [||], _) -> (Intcode input)
+    | (Add, [|x;y;p|], next) ->
+        set p (get x + get y) |> ignore
+        run (Intcode input) next
+    | (Multiply, [|x;y;p|], next) ->
+        set p (get x * get y) |> ignore
+        run (Intcode input) next
+    | (op, args, next) ->        
+        sprintf "Unknown operator: %O on %s" op (System.String.Join(',', args))|> failwith
+
+let runValue (Intcode input) noun verb =    
+    Array.set input 1 noun
+    Array.set input 2 verb
+    run (Array.copy input |> Intcode) 0 |> fun (Intcode x) -> x |> Array.head
+
+let bruteForce intcode desired =    
+    let options = seq {
+        for n in 0..100 do
+            for v in 0..100 do
+                yield (n,v)
+    }
+
+    let isMatch (n,v) =         
+        try
+            let result = runValue intcode n v           
+            result = desired
+        with
+        | _ -> false
+
+    Seq.tryFind isMatch options
+            
