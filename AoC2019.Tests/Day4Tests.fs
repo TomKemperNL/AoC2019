46fd26d954ee23bb8d6bfe33658c0835d0b14766
Tom Kemper <tomkempernl@gmail.com>
2019-12-07 09:47:30 +0100

    Bit stuck on Day 4B

--- AoC2019.Tests/Day4Tests.fs
+++ AoC2019.Tests/Day4Tests.fs
@@ -5,14 +5,59 @@ open FsUnit
 
 [<Test>]
 let ``Day4 Example`` () =     
-    let bounds = (100000, 999999)
-    isCandidate bounds 111111 |> should equal true
-    isCandidate bounds 223450 |> should equal false
-    isCandidate bounds 123789 |> should equal false
+    let rules = [
+        isInBounds (100000, 999999);
+        isNDigits 6;
+        hasTwoAdjacent;
+        isIncreasing;
+    ]
     
+    isCandidate rules 111111 |> should equal true
+    isCandidate rules 223450 |> should equal false
+    isCandidate rules 123789 |> should equal false
 
-let input = (108457, 562041)
 [<Test>]
 let ``Day4 A`` () =
-    let (min, max) = input
-    seq { min .. max } |> Seq.filter (isCandidate input) |> Seq.length |> should equal 2779
+    let (min, max) = (108457, 562041)
+
+    let rules = [
+        isInBounds (min, max);
+        isNDigits 6;
+        hasTwoAdjacent;
+        isIncreasing;
+    ]
+
+    seq { min .. max } |> Seq.filter (isCandidate rules) |> Seq.length |> should equal 2779
+
+[<Test>]
+let ``Has Three Adjacent`` () =
+    hasThreeAdjacent 112211 |> should equal false
+    hasThreeAdjacent 111223 |> should equal true
+    hasThreeAdjacent 112223 |> should equal true
+
+[<Test>]
+let ``Has Exactly two Adjacent`` () =
+    hasExactlyTwoAdjacent 112233 |> should equal true
+    hasExactlyTwoAdjacent 123444 |> should equal false
+    hasExactlyTwoAdjacent 444123 |> should equal false
+    hasExactlyTwoAdjacent 111122 |> should equal true
+
+[<Test>]
+let ``Can convert number to list`` () =
+    toList 112211 |> should equivalent [1;1;2;2;1;1;]
+
+[<Test>]
+let ``Day4 B`` () =
+    let (min, max) = (108457, 562041)
+
+    let rules = [
+        isInBounds (min, max);
+        isNDigits 6;
+        hasTwoAdjacent;
+        isIncreasing;
+        hasExactlyTwoAdjacent
+    ]
+
+    let result = seq { min .. max } |> Seq.filter (isCandidate rules) |> Seq.length     
+    result |> should greaterThan 1444    
+    result  |> should greaterThan 1847
--- AoC2019/Day4.fs
+++ AoC2019/Day4.fs
@@ -1,16 +1,54 @@
 ﻿module AoC2019.Day4
 
+open AoC2019.Shared
+
 type Bounds = int*int
 
+let toList x = 
+    x.ToString() |> Seq.map (string >> int) |> Seq.toList
 
-let isCandidate (lower,upper) (x: int) =
-    let isInBounds = (x >= lower) && (x <= upper)
-    let isSixDigits = (x.ToString().Length = 6)
-    let xAsList = x.ToString() |> Seq.map int |> Seq.toList
-    let isIncreasing = (List.sort xAsList) = xAsList    
-    let hasTwoAdjacent = 
-        List.zip (List.skip 1 xAsList) (List.take 5 xAsList) 
-            |> List.exists (fun (x,y) -> (x = y))        
+let isInBounds (lower, upper) x = (x >= lower) && (x <= upper)
+let isNDigits n x = (x.ToString().Length = n)
+let isIncreasing x = 
+    let xAsList = toList x
+    (List.sort xAsList) = xAsList
 
-    (isInBounds && isSixDigits && hasTwoAdjacent && isIncreasing)
+let hasTwoAdjacent x = 
+    let xAsList = toList x
+    List.zip (List.skip 1 xAsList) (List.take 5 xAsList) 
+        |> List.exists (fun (x,y) -> (x = y))
+        
+
+let hasThreeAdjacent x =
+    let xAsList = toList x    
+    let length = List.length xAsList
+
+    let left = List.take (length - 2) xAsList
+    let middle = (List.skip 1 >> List.take (length - 2)) xAsList 
+    let right = List.skip 2 xAsList
+
+    List.zip3 left middle right |> List.exists (fun (x,y,z) -> (x = y) && (y = z))
+
+let hasExactlyTwoAdjacent x =
+    let xAsList = toList x    
+    let pairs = seq {
+        let list = new System.Collections.Generic.List<int>()
+        let mutable current = -1
+        for i in xAsList do
+            if i <> current then
+                if list.Count >= 2 then
+                    yield list
+                list.Clear()
+            else 
+                if list.Count < 2 then
+                    list.Add(i)
+                else 
+                    list.Clear()
+            current <- i
+            list.Add i
+    }
+    not <| Seq.isEmpty pairs
     
+
+let isCandidate rules (x: int) =
+    List.forall (fun r -> r x) rules 
