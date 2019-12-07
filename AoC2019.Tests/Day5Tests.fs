5d969462b8a89a0b85aba1ee2c130cdeeac7a0c4
Tom Kemper <tomkempernl@gmail.com>
2019-12-07 17:46:10 +0100

    IO for Intcode

---
+++ AoC2019.Tests/Day5Tests.fs
@@ -0,0 +1,15 @@
+﻿module AoC2019.Day5.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open AoC2019.Intcode
+
+[<Test>]
+let ``Day5 echo IO`` () =     
+    let input () = 5
+    let mutable out = -1
+    let output x = 
+        out <- x
+    run (Program [3;0;4;0;99]) 0 input output |> ignore
+    out |> should equal 5
--- AoC2019/Intcode.fs
+++ AoC2019/Intcode.fs
@@ -8,39 +8,55 @@ type Opcode =
     | Halt
     | Add
     | Multiply
+    | Input
+    | Output
     
 let mapping = dict [
     (99, Halt);
     (1, Add);
-    (2, Multiply)]
+    (2, Multiply);
+    (3, Input);
+    (4, Output);
+]
 
 let arity = dict [
     (Halt, 0);
     (Add, 3);
-    (Multiply, 3)
+    (Multiply, 3);
+    (Input, 1);
+    (Output, 1)
 ]
 
-let run (Program input) pos =
-    let inputArray = Array.ofList input
+let noInput = fun() -> failwith "No Input Configured"
+let noOutput = fun x -> failwith "No Output Configured"
+
+let run (Program ints) (pos: int) (input: Input) (output: Output) =
+    let inputArray = Array.ofList ints
     let set = Array.set inputArray
     let get = Array.get inputArray
-    let parseOpcode pos = 
-        let op =  mapping.[get pos]
+    let parseOpcode x = 
+        let op =  mapping.[get x]
         let arity = arity.[op]
         if arity > 0 then        
-            (op, Array.sub inputArray (pos + 1) arity, pos + 1 + arity)
+            (op, Array.sub inputArray (x + 1) arity, x + 1 + arity)
         else 
-            (op, [||], pos)
+            (op, [||], x)
 
-    let rec runArray input pos = 
+    let rec runArray inputArray pos = 
         match parseOpcode pos with
-        | (Halt, [||], _) -> input
+        | (Halt, [||], _) -> inputArray
         | (Add, [|x;y;p|], next) ->
             set p (get x + get y) |> ignore
-            runArray input next
+            runArray inputArray next
         | (Multiply, [|x;y;p|], next) ->
             set p (get x * get y) |> ignore
-            runArray input next
+            runArray inputArray next
+        | (Input, [|p|], next) ->
+            set p (input()) |> ignore
+            runArray inputArray next
+        | (Output, [|p|], next) ->
+            output (get p)
+            runArray inputArray next
         | (op, args, next) ->        
             sprintf "Unknown operator: %O on %s" op (System.String.Join(',', args))|> failwith
 
