338e52d923a59e9fd535891983ed7dc2e1eb5060
Tom Kemper <tomkempernl@gmail.com>
2019-12-07 08:10:47 +0100

    Day 4A

---
+++ AoC2019.Tests/Day4Tests.fs
@@ -0,0 +1,18 @@
+﻿module AoC2019.Day4.Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Day4 Example`` () =     
+    let bounds = (100000, 999999)
+    isCandidate bounds 111111 |> should equal true
+    isCandidate bounds 223450 |> should equal false
+    isCandidate bounds 123789 |> should equal false
+    
+
+let input = (108457, 562041)
+[<Test>]
+let ``Day4 A`` () =
+    let (min, max) = input
+    seq { min .. max } |> Seq.filter (isCandidate input) |> Seq.length |> should equal 2779
---
+++ AoC2019/Day4.fs
@@ -0,0 +1,16 @@
+﻿module AoC2019.Day4
+
+type Bounds = int*int
+
+
+let isCandidate (lower,upper) (x: int) =
+    let isInBounds = (x >= lower) && (x <= upper)
+    let isSixDigits = (x.ToString().Length = 6)
+    let xAsList = x.ToString() |> Seq.map int |> Seq.toList
+    let isIncreasing = (List.sort xAsList) = xAsList    
+    let hasTwoAdjacent = 
+        List.zip (List.skip 1 xAsList) (List.take 5 xAsList) 
+            |> List.exists (fun (x,y) -> (x = y))        
+
+    (isInBounds && isSixDigits && hasTwoAdjacent && isIncreasing)
+    
