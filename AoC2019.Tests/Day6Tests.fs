009aa2e914806aafe778a9b8b668a226683d3261
Tom <tomkempernl@gmail.com>
2019-12-10 17:06:30 +0100

    Day 6A

---
+++ AoC2019.Tests/Day6Tests.fs
@@ -0,0 +1,49 @@
+﻿module AoC2019.Day6.Tests
+
+
+open NUnit.Framework
+open FsUnit
+open System.IO
+
+let parse = 
+    Seq.map (fun (s: string) ->  s.Split(')')) >> Seq.map (fun [|a;b|] -> (OrbitObject a, OrbitObject b))
+
+
+let input = 
+    File.ReadAllLines "./Dag6Input.txt"
+        |> parse
+
+
+[<Test>]
+let ``Day6 examples`` () =  
+    let example = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L"""
+    let split = example.Split(System.Environment.NewLine)
+    
+    let map = createMap (parse split)
+    System.Console.WriteLine(map)
+
+    let huh = Map.find (OrbitObject "D") map
+    huh |> should equal (OrbitObject "C")
+
+    orbits map (OrbitObject "D") |> Set.count |> should equal 3
+    orbits map (OrbitObject "L") |> Set.count |> should equal 7
+    orbits map (OrbitObject "COM") |> Set.count |> should equal 0
+
+
+    totals map |> should equal 42
+
+
+[<Test>]
+let ``Day6 A`` () = 
+    let map = createMap input
+    totals map |> should equal 0
---
+++ AoC2019/Day6.fs
@@ -0,0 +1,49 @@
+﻿module AoC2019.Day6
+
+type OrbitObject =
+    OrbitObject of string
+
+type OrbitMap = 
+    Map<OrbitObject, Set<OrbitObject>>
+
+let createReverseMap pairs =    
+    let updateMap map (center,satelite) =         
+        match Map.tryFind center map with
+        | Some orbits ->             
+            Map.add center (Set.add satelite orbits) map
+        | None -> 
+            Map.add center (Set.singleton satelite) map            
+    Seq.fold updateMap Map.empty pairs 
+
+
+let createMap pairs =  
+    let flip (a,b) = (b,a)
+    Seq.map flip pairs |> Map.ofSeq 
+    
+let memoize fn =
+  let cache = new System.Collections.Generic.Dictionary<_,_>()
+  (fun x ->
+    match cache.TryGetValue x with
+    | true, v -> v
+    | false, _ -> let v = fn (x)
+                  cache.Add(x,v)
+                  v)
+
+let rec isOrbitedBy map o =
+    let directOrbits = Map.tryFind o map
+    match directOrbits with 
+    | Some directOrbits ->
+        let indirectOrbits = Set.unionMany (Set.map (isOrbitedBy map) directOrbits)
+        Set.union directOrbits indirectOrbits
+    | None -> Set.empty
+
+let rec orbits map o = 
+    let center = Map.tryFind o map
+    match center with 
+    | Some center ->
+        Set.add center (orbits map center)
+    | None ->
+        Set.empty
+
+let totals map =
+    Map.toSeq map |> Seq.sumBy (fun (k, v) -> orbits map k |> Set.count)
