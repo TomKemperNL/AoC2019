f29c5a04a60f5b2c580d203cc3d94ecd4fd96f4f
Tom <tomkempernl@gmail.com>
2019-12-13 17:16:22 +0100

    work-in-progress

--- AoC2019.Tests/Day7Tests.fs
+++ AoC2019.Tests/Day7Tests.fs
@@ -28,4 +28,13 @@ let program =
 
 [<Test>]
 let ``Day 7A`` () =
-    maxThruster program |> should equal 0
+    maxThruster program |> should equal 65464
+
+[<Test>]
+let ``Day 7B Example`` () = 
+    let program = Program [3;26;1001;26;-4;26;3;27;1002;27;2;27;1;27;26;27;4;27;1001;28;-1;28;1005;28;6;99;0;0;5]
+    maxThrusterLoop program |> should equal 139629729
+
+//[<Test>]
+let ``Day 7B`` () =
+    maxThrusterLoop program |> should equal 0
