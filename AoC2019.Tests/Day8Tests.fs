3a4dc01af303f2fd1925614db4bf3abde31eb940
Tom Kemper <tomkempernl@gmail.com>
2019-12-15 20:50:28 +0100

    Day 8 B

---
+++ AoC2019.Tests/Day8Tests.fs
@@ -0,0 +1,26 @@
+﻿module AoC2019.Day8.Tests
+
+open NUnit.Framework
+open FsUnit
+open System.IO
+
+open AoC2019.Shared
+
+[<Test>]
+let ``Day 8 Examples`` () =
+    checksum 3 2 (toList 123456789012L) |> should equal 1
+
+let input = File.ReadAllText("./Dag8Input.txt").ToCharArray()
+                |> Array.map string
+                |> Array.map System.Int32.Parse
+                |> List.ofArray
+
+[<Test>]
+let ``Day 8 A`` () =                
+    checksum 25 6 input |> should equal 1620
+
+
+[<Test>]
+let ``Day 8 B`` () =                
+    splitInLayers 25 6 input |> flatten |> (print 25)
+    //BCYEF
---
+++ AoC2019/Day8.fs
@@ -0,0 +1,77 @@
+﻿module AoC2019.Day8
+
+open AoC2019.Shared
+
+type Layer = int array
+type Image = Layer list
+
+let private splitInParts initial partLength input =
+    seq {
+        let items = Array.init (partLength) (constant initial)
+        let mutable count = 0
+        for x in input do
+            Array.set items count x
+            if count = (items.Length - 1) then
+                yield Array.copy items
+                count <- 0
+            else
+                count <- count + 1
+    }
+
+let splitInLayers width height (input: int seq) : Image = 
+    splitInParts 0 (width * height) input |> List.ofSeq
+
+let rec flatten (image: Image) : Layer = 
+    let pickColor ((upper, lower): int*int) = 
+        match upper with
+        | 0 | 1 -> 
+            upper
+        | 2 -> 
+            lower
+        | _ -> 
+            failwith <| sprintf "Unknown color %i" upper
+
+    match image with    
+    | [] -> Array.empty
+    | [x] -> x
+    | h :: t ->
+        let layer = flatten t
+        Array.zip h layer |> Array.map pickColor
+        
+let print width (layer: Layer) : unit = 
+    let mapCharacter x = 
+        match x with 
+        | 0 -> "   "
+        | 1 -> " X "
+        | 2 -> "   "
+        | _ -> " ? "
+
+    let printLine (line: string array) : unit =
+        System.String.Join("", line) |> printfn "%s" |> ignore
+
+    let layer = Array.map mapCharacter layer
+    let lines = splitInParts " " width layer 
+    Seq.iter printLine lines
+    
+let leastZerosLayer (layers: Image) = 
+    let zerosInLayer layer = 
+        Seq.filter (fun x -> x = 0) layer |> Seq.length
+
+    List.minBy zerosInLayer layers
+    
+let multiplyLayer (layer: Layer) = 
+    let gather (ones, twos) item = 
+        match item with 
+        | 1 -> (ones, twos + 1)
+        | 2 -> (ones + 1, twos)
+        | _ -> (ones, twos)
+
+    let (ones, twos) = Array.fold gather (0,0) layer
+    ones * twos
+
+
+
+let checksum width height (input: int seq) =
+    let layers = splitInLayers width height input
+    let layer = leastZerosLayer layers
+    multiplyLayer layer
